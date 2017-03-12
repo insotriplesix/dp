37f3958a83a9de5e3930e87ad4cbf871e6818200
Roman <rtereshkov@gmail.com>
2017-03-12 13:12:15 +0700

    Create prog.cpp

---
+++ CPP/CppStudio/Beginner/task5/prog.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << (char)201 << (char)203 << (char)203 << (char)203 <<
+		(char)203 << (char)203 << (char)203 << (char)187 << std::endl;
+	std::cout << (char)200 << (char)202 << (char)202 << (char)202 <<
+		(char)202 << (char)202 << (char)202 << (char)188 << std::endl;
+
+	return 0;
+}
