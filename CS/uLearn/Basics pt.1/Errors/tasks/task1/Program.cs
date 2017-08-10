85037a775cb318b866eaa53db69375dba42d85fa
Roman <rtereshkov@gmail.com>
2017-08-10 20:20:56 +0700

    Add files via upload

---
+++ CS/uLearn/Basics pt.1/Errors/tasks/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var sum = 0;
+            for (int i = 0; i < 1000; ++i)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
