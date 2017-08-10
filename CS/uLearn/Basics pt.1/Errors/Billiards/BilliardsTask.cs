ebe9c6666fe7aab0b947d55f2873fe6cc56953ba
Roman <rtereshkov@gmail.com>
2017-08-10 21:33:57 +0700

    Add files via upload

---
+++ CS/uLearn/Basics pt.1/Errors/Billiards/BilliardsTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Billiards
+{
+	public static class BilliardsTask
+	{
+		public static double BounceWall(double directionRadians, double wallInclinationRadians)
+		{
+            if (wallInclinationRadians * 180 / Math.PI == 90)
+                return Math.PI - directionRadians;
+            else if (wallInclinationRadians == 0)
+                return 2 * Math.PI - directionRadians;
+            else
+                return 2 * wallInclinationRadians - directionRadians;
+        }
+	}
+}
