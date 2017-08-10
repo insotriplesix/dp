d21c5ff047bb184466abfacd611bba8378e213b0
Roman <rtereshkov@gmail.com>
2017-08-10 19:46:18 +0700

    Add files via upload

---
+++ CS/uLearn/Basics pt.1/First Meeting/tasks/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input a: ");
+            var a = int.Parse(Console.ReadLine());
+            Console.Write("Input b: ");
+            var b = int.Parse(Console.ReadLine());
+
+            // 1st case
+            var tmp = b;
+            b = a;
+            a = tmp;
+
+            Console.WriteLine("a is " + a.ToString() + " now");
+            Console.WriteLine("b is " + b.ToString() + " now");
+         
+            Console.Write("Input a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input b: ");
+            b = int.Parse(Console.ReadLine());
+
+            // 2nd case
+            a ^= b;
+            b ^= a;
+            a ^= b;
+
+            Console.WriteLine("a is " + a.ToString() + " now");
+            Console.WriteLine("b is " + b.ToString() + " now");
+        }
+    }
+}
