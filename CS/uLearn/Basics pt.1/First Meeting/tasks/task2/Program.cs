a16e9b3f430f9b9b858a438460bec16c1f60d301
Roman <rtereshkov@gmail.com>
2017-08-10 19:49:24 +0700

    Add files via upload

---
+++ CS/uLearn/Basics pt.1/First Meeting/tasks/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input value: ");
+            var value = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.Write(value % 10);
+                value /= 10;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
