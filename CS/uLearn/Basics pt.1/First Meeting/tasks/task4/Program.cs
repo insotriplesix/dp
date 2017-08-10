e5f7bf99eac11450cd450d7b6a16b6ca63197fb8
Roman <rtereshkov@gmail.com>
2017-08-10 20:10:22 +0700

    Add files via upload

---
+++ CS/uLearn/Basics pt.1/First Meeting/tasks/task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input n: ");
+            var n = int.Parse(Console.ReadLine());
+            Console.Write("Input x: ");
+            var x = int.Parse(Console.ReadLine());
+            Console.Write("Input y: ");
+            var y = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            for (int i = 1; i < n; ++i)
+            {
+                if (i % x == 0 && i % y == 0)
+                {
+                    ++counter;
+                    Console.Write(i.ToString() + " ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(counter);
+        }
+    }
+}
