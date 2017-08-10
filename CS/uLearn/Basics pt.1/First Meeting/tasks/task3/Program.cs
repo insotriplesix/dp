dc7c67483c7b20ffcff072dfcc14091c0f87eabf
Roman <rtereshkov@gmail.com>
2017-08-10 19:59:21 +0700

    Add files via upload

---
+++ CS/uLearn/Basics pt.1/First Meeting/tasks/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input H value: ");
+            var h = int.Parse(Console.ReadLine());
+            var angle = 30;
+            Console.WriteLine(angle * h < 180 ? angle * h : 360 - Math.Abs(12 - h) * angle);          
+        }
+    }
+}
