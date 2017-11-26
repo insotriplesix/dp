67ecc5b157bba91d0799256e5af7769398eaba25
Roman <rtereshkov@gmail.com>
2017-11-27 00:55:20 +0700

    Create HistogramTask.cs

---
+++ uLearn/Basics pt.1/Arrays/Histogram/HistogramTask.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+namespace Names
+{
+  internal static class HistogramTask
+  {
+    public static HistogramData GetHistogramBirthsPerDay(NameData[] names, string name)
+    {
+        var numberOfDays = 31;
+        var daysNumbers = new string[numberOfDays];
+        for (int i = 0; i < numberOfDays; i++)
+            daysNumbers[i] = (i + 1).ToString();
+
+        var birthsCounts = new double[numberOfDays];
+        foreach (var person in names)
+        {
+            if (person.Name == name && person.BirthDate.Day != 1)
+                birthsCounts[person.BirthDate.Day - 1]++;
+        }
+
+        return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name),
+            daysNumbers, birthsCounts);
+    }
+  }
+}
