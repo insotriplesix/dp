40c05116a7b3811c2a594a79eedc8e43148984b9
5aboteur <5aboteur@protonmail.com>
2018-03-10 22:29:15 +0000

    raw

--- eltex/task5/pipe/main.c
+++ eltex/task5/pipe/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-// implement 'ls -l | cut -c 48-' routine
+// implement 'ls -l | cut -c 48-' bash cmd
 
+#define P_RD 0
+#define P_WR 1
 #define BUFSIZE 1024
 
 int
@@ -18,28 +21,81 @@ main(void)
 		exit(EXIT_FAILURE);
 	}
 
+	puts("\n\033[1;33mTest bash output:\033[0m");
+	system("ls -l | cut -c 48-");
+	puts("\n\033[1;33mProgram output:\033[0m");
+
+	/////////////////////////////////////////////////////
+
 	pid_t pid = fork();
 
 	switch (pid) {
 	case -1:
 		fprintf(stderr, "Cannot create child process.\n");
+		close(fd[P_RD]);
+		close(fd[P_WR]);
 		exit(EXIT_FAILURE);
 	case 0:
-		close(fd[1]);
-		nbytes = read(fd[0], data, BUFSIZE);
-		close(fd[0]);
-		printf("\nBytes received: 0x%zx\nOutput:\n----------\n%s\n",
-			nbytes, data);
-		break;
-	default:
-		close(fd[0]);
-		if (dup2(fd[1], STDOUT_FILENO) == -1) {
+		// pipe: -> [BACK, FRNT]
+		if (dup2(fd[P_WR], STDOUT_FILENO) == -1) {
 			fprintf(stderr, "Error fd duplication.\n");
 			exit(EXIT_FAILURE);
 		}
-		close(fd[1]);
+
+		close(fd[P_RD]);
+		close(fd[P_WR]);
+
 		execlp("ls", "ls", "-la", (char *) NULL);
 		exit(EXIT_SUCCESS);
+	default:
+		wait(&pid);
+	}
+
+	pid = fork();
+
+	switch (pid) {
+	case -1:
+		fprintf(stderr, "Cannot create child process.\n");
+		close(fd[P_RD]);
+		close(fd[P_WR]);
+		exit(EXIT_FAILURE);
+	case 0:
+		// pipe: [BACK, FRNT] <-
+		if (dup2(fd[P_RD], STDIN_FILENO) == -1) {
+			fprintf(stderr, "Error fd duplication.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		// pipe: -> [BACK, FRNT]
+		if (dup2(fd[P_WR], STDOUT_FILENO) == -1) {
+			fprintf(stderr, "Error fd duplication.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		close(fd[P_RD]);
+		close(fd[P_WR]);
+
+		nbytes = read(fd[P_RD], data, BUFSIZE);
+
+		close(fd[P_RD]);
+		close(fd[P_WR]);
+
+		printf("\nBytes received: 0x%zx\nData:\n-----\n%s\n",
+			nbytes, data);
+//		execlp("cut", "cut", "-c", "48-", (char *) NULL);
+		exit(EXIT_SUCCESS);
+	default:
+		wait(&pid);
+
+		// pipe: [BACK, FRNT] <-
+		nbytes = read(fd[P_RD], data, BUFSIZE);
+
+		close(fd[P_RD]);
+		close(fd[P_WR]);
+
+		printf("\nBytes received: 0x%zx\nData:\n-----\n%s\n",
+			nbytes, data);
+		break;
 	}
 
 	return 0;
