609f03682830a02708b6c8712cd0bdcdae19dc06
5aboteur <5aboteur@protonmail.com>
2018-03-16 19:21:27 +0000

    impl tcp default echo server

---
+++ eltex/task6/tcp_echo_def/server.c
@@ -0,0 +1,124 @@
+#include "tcp_echo.h"
+
+static int sockfd;
+
+int
+main(int argc, char *argv[])
+{
+	int bytes, optval, listenfd, rc, clnt_len, serv_len;
+	struct hostent *clnt_host;
+	struct sockaddr_in clnt_addr, serv_addr;
+
+	clnt_len = sizeof(clnt_addr);
+	serv_len = sizeof(serv_addr);
+
+	if (argc < 2) {
+		port = _DEF_PORT;
+		printf(_RED_CLR"Using default port: %d\n"_DEF_CLR, port);
+	} else
+		port = atoi(argv[1]);
+
+	listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenfd < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	optval = 1;
+
+	rc = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+		(const void *) &optval, sizeof(optval));
+	if (rc < 0) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	memset((char *) &serv_addr, 0, serv_len);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(port);
+
+	rc = bind(listenfd, (struct sockaddr *) &serv_addr, serv_len);
+	if (rc < 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	rc = getsockname(listenfd, (struct sockaddr *) &serv_addr,
+		(socklen_t *) &serv_len);
+	if (rc < 0) {
+		perror("getsockname");
+		exit(EXIT_FAILURE);
+	}
+
+	printf(_BLUE_CLR"Server:"_DEF_CLR" port -- %d\n",
+		ntohs(serv_addr.sin_port));
+
+	rc = listen(listenfd, 1);
+	if (rc < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	sockfd = accept(listenfd, (struct sockaddr *) &clnt_addr,
+		(socklen_t *) &clnt_len);
+	if (sockfd < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	clnt_host = gethostbyaddr((const char *) &clnt_addr.sin_addr.s_addr,
+		sizeof(clnt_addr.sin_addr.s_addr), AF_INET);
+	if (clnt_host == NULL) {
+		perror("gethostbyaddr");
+		exit(EXIT_FAILURE);
+	}
+
+	close(listenfd);
+
+	printf(_BLUE_CLR"Server:"_DEF_CLR" established connection with %s ~> %s:%d\n",
+		clnt_host->h_name, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+
+	signal(SIGCHLD, (__sighandler_t) killproc);
+
+	while (0x1) {
+		char chkmsg[MSGSIZ];
+
+		msggen(chkmsg);
+
+		bytes = send(sockfd, chkmsg, MSGSIZ, 0);
+		if (bytes < 0) {
+			perror("send");
+			exit(EXIT_FAILURE);
+		}
+
+		bytes = recv(sockfd, chkmsg, MSGSIZ, 0);
+		if (bytes < 0) {
+			perror("recv");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return 0;
+}
+
+void
+killproc(void)
+{
+	int wstatus = 0;
+	wait(&wstatus);
+	close(sockfd);
+}
+
+void
+msggen(char *msg)
+{
+	int start = (int)' ';
+	int end = (int)'~';
+	int i = 0;
+
+	for (; i < MSGSIZ - 1; ++i)
+		msg[i] = (char)(start + rand() % (end - start));
+
+	msg[i] = '\0';
+}
---
+++ eltex/task6/tcp_echo_def/tcp_echo.h
@@ -0,0 +1,37 @@
+#ifndef __TCP_ECHO_H__
+#define __TCP_ECHO_H__
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MSGSIZ 64
+
+#define _DEF_PORT 6666
+
+#define _DEF_CLR "\033[0m"
+#define _BLACK_CLR "\033[1;30m"
+#define _RED_CLR "\033[1;31m"
+#define _GREEN_CLR "\033[1;32m"
+#define _YELLOW_CLR "\033[1;33m"
+#define _BLUE_CLR "\033[1;34m"
+#define _MAGENTA_CLR "\033[1;35m"
+#define _CYAN_CLR "\033[1;36m"
+#define _WHITE_CLR "\033[1;37m"
+
+int port;
+
+void killproc(void);
+void msggen(char *msg);
+
+#endif
