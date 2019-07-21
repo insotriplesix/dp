8aca06e27d17d34d4d13ac40f30f0fb458148318
5aboteur <5aboteur@protonmail.com>
2019-07-21 21:44:27 +0700

    Build simple RESTful web service

---
+++ spring/restful-service/src/main/java/com/example/restfulservice/RestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulServiceApplication.class, args);
+	}
+
+}
