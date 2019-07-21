a78276c17fcb221f333c5079db39545751106631
5aboteur <5aboteur@protonmail.com>
2019-07-21 13:02:15 +0700

    Add test Spring project

---
+++ spring/test-spring/src/test/java/io/github/aboteur/testSpring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aboteur.testSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
