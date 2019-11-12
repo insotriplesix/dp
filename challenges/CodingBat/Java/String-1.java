7b5f43120303976137211b0450f3296a4a0469e0
5aboteur <5aboteur@protonmail.com>
2019-11-13 01:09:03 +0700

    Add another few CodingBat String-1 solutions

--- challenges/CodingBat/Java/String-1.java
+++ challenges/CodingBat/Java/String-1.java
@@ -96,3 +96,214 @@ public String comboString(String a, String b) {
   return lenA <= lenB ? a + b + a : b + a + b;
 }
 
+
+/*
+Given 2 strings, return their concatenation, except omit the first char of each. The strings will be
+at least length 1.
+*/
+
+public String nonStart(String a, String b) {
+  return a.substring(1, a.length()) + b.substring(1, b.length());
+}
+
+
+/*
+Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The
+string length will be at least 2.
+*/
+
+public String left2(String str) {
+  return str.substring(2, str.length()) + str.substring(0, 2);
+}
+
+
+/*
+Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
+The string length will be at least 2.
+*/
+
+public String right2(String str) {
+  int len = str.length();
+  return str.substring(len - 2, len) + str.substring(0, len - 2);
+}
+
+
+/*
+Given a string, return a string length 1 from its front, unless front is false, in which case return
+a string length 1 from its back. The string will be non-empty.
+*/
+
+public String theEnd(String str, boolean front) {
+  return front ? str.substring(0, 1) : str.substring(str.length() - 1, str.length());
+}
+
+
+/*
+Given a string, return a version without both the first and last char of the string. The string may
+be any length, including 0.
+*/
+
+public String withouEnd2(String str) {
+  int len = str.length();
+  if (len < 3) return "";
+  return str.substring(1, len - 1);
+}
+
+
+/*
+Given a string of even length, return a string made of the middle two chars, so the string "string"
+yields "ri". The string length will be at least 2.
+*/
+
+public String middleTwo(String str) {
+  int len = str.length();
+  return str.substring(len / 2 - 1, len / 2 + 1);
+}
+
+
+/*
+Given a string, return true if it ends in "ly".
+*/
+
+public boolean endsLy(String str) {
+  int len = str.length();
+  if (len < 2) return false;
+  return str.substring(len - 2, len).equals("ly");
+}
+
+
+/*
+Given a string and an int n, return a string made of the first and last n chars from the string.
+The string length will be at least n.
+*/
+
+public String nTwice(String str, int n) {
+  int len = str.length();
+  return str.substring(0, n) + str.substring(len - n, len);
+}
+
+
+/*
+Given a string and an index, return a string length 2 starting at the given index. If the index is
+too big or too small to define a string length 2, use the first 2 chars. The string length will be
+at least 2.
+*/
+
+public String twoChar(String str, int index) {
+  int len = str.length();
+  return (index < 0 || len - index < 2)
+    ? str.substring(0, 2)
+    : str.substring(index, index + 2);
+}
+
+
+/*
+Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and".
+The string length will be at least 3.
+*/
+
+public String middleThree(String str) {
+  int len = str.length();
+  return str.substring(len / 2 - 1, len / 2 + 2);
+}
+
+
+/*
+Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with
+"badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0. Note: use
+.equals() to compare 2 strings.
+*/
+
+public boolean hasBad(String str) {
+  int len = str.length();
+  if (len < 3) return false;
+
+  return str.substring(0, 3).equals("bad")
+    || (len > 3 && str.substring(1, 4).equals("bad"));
+}
+
+
+/*
+Given a string, return a string length 2 made of its first 2 chars. If the string length is less
+than 2, use '@' for the missing chars.
+*/
+
+public String atFirst(String str) {
+  int len = str.length();
+  switch (len) {
+    case 2: return str;
+    case 1: return str + "@";
+    case 0: return "@@";
+    default: return str.substring(0, 2);
+  }
+}
+
+
+/*
+Given 2 strings, a and b, return a new string made of the first char of a and the last char of b,
+so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+*/
+
+public String lastChars(String a, String b) {
+  int lenA = a.length();
+  int lenB = b.length();
+
+  String result = "";
+
+  switch (lenA) {
+    case 0: result += "@"; break;
+    default: result += a.substring(0, 1);
+  }
+
+  switch (lenB) {
+    case 0: result += "@"; break;
+    default: result += b.substring(lenB - 1, lenB);
+  }
+
+  return result;
+}
+
+
+/*
+Given two strings, append them together (known as "concatenation") and return the result. However,
+if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields
+"abcat".
+*/
+
+public String conCat(String a, String b) {
+  int lenA = a.length();
+  int lenB = b.length();
+
+  String result = "";
+
+  switch (lenA) {
+    case 0: return b;
+    default: result += a;
+  }
+
+  switch (lenB) {
+    case 0: return a;
+    default:
+      if (b.charAt(0) == a.charAt(lenA - 1) && lenB > 1)
+          result += b.substring(1, lenB);
+      else if (b.charAt(0) != a.charAt(lenA - 1))
+        result += b;
+  }
+
+  return result;
+}
+
+
+/*
+Given a string of any length, return a new string where the last 2 chars, if present, are swapped,
+so "coding" yields "codign".
+*/
+
+public String lastTwo(String str) {
+  int len = str.length();
+  if (len < 2) return str;
+  return str.substring(0, len - 2)
+    + str.substring(len - 1, len)
+    + str.substring(len - 2, len - 1);
+}
+
