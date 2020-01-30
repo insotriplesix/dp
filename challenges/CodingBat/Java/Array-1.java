f6a7f39c89a0e0e037783351c637d3755f7669f8
5aboteur <5aboteur@protonmail.com>
2020-01-31 00:13:52 +0700

    Add solutions for array-1 codingbat

--- challenges/CodingBat/Java/Array-1.java
+++ challenges/CodingBat/Java/Array-1.java
@@ -62,3 +62,45 @@ public int[] rotateLeft3(int[] nums) {
 }
 
 
+/*
+Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3}
+becomes {3, 2, 1}.
+*/
+
+public int[] reverse3(int[] nums) {
+  int[] result = new int[nums.length];
+  for (int j = 0, i = nums.length - 1; i >= 0; j++, i--)
+    result[j] = nums[i];
+  return result;
+}
+
+
+/*
+Given an array of ints length 3, figure out which is larger, the first or last element in the array,
+and set all the other elements to be that value. Return the changed array.
+*/
+
+public int[] maxEnd3(int[] nums) {
+  int max = nums[0] > nums[nums.length - 1] ? nums[0] : nums[nums.length - 1];
+  int[] result = new int[nums.length];
+  for (int i = 0; i < nums.length; i++)
+    result[i] = max;
+  return result;
+}
+
+
+/*
+Given an array of ints, return the sum of the first 2 elements in the array. If the array length is
+less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+*/
+
+public int sum2(int[] nums) {
+  int sum = 0;
+  int len = nums.length >= 2 ? 2 : nums.length;
+  for (int i = 0; i < len; i++)
+    sum += nums[i];
+  return sum;
+}
+
+
+
