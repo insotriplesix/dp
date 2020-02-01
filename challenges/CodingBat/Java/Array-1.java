9c6e6e468f3d92d7cdbe7bb7891d9953b0c10a59
5aboteur <5aboteur@protonmail.com>
2020-02-01 23:53:34 +0700

    Add fix23 of array-1 codingbat

--- challenges/CodingBat/Java/Array-1.java
+++ challenges/CodingBat/Java/Array-1.java
@@ -183,3 +183,20 @@ public boolean double23(int[] nums) {
   return false;
 }
 
+
+/*
+Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3
+element to 0. Return the changed array.
+*/
+
+public int[] fix23(int[] nums) {
+  int prev = 0;
+
+  for (int i = 1; i < nums.length; i++) {
+    if (nums[i - 1] == 2 && nums[i] == 3)
+      nums[i] = 0;
+  }
+
+  return nums;
+}
+
