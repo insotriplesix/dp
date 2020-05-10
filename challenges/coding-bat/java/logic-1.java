3769e38b8884e0490a58267bf3c8a9293d73fc14
5aboteur <5aboteur@protonmail.com>
2020-05-11 01:58:30 +0700

    Add another solutions for logic-1

--- challenges/coding-bat/java/logic-1.java
+++ challenges/coding-bat/java/logic-1.java
@@ -43,3 +43,121 @@ public boolean squirrelPlay(int temp, boolean isSummer) {
   return isSummer ? (temp >= 60 && temp <= 100) : (temp >= 60 && temp <= 90);
 }
 
+
+/*
+You are driving a little too fast, and a police officer stops you. Write code to compute the result,
+encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result
+is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is
+2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+*/
+
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  int noTicketMax = isBirthday ? 65 : 60;
+  int smallTicketMax = isBirthday ? 85 : 80;
+
+  return speed <= noTicketMax ? 0 : (speed <= smallTicketMax ? 1 : 2);
+}
+
+
+/*
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden,
+so in that case just return 20.
+*/
+
+public int sortaSum(int a, int b) {
+  int sum = a + b;
+  return (sum >= 10 && sum <= 19) ? 20 : sum;
+}
+
+
+/*
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are
+on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays,
+the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then
+on weekdays it should be "10:00" and weekends it should be "off".
+*/
+
+public String alarmClock(int day, boolean vacation) {
+  if (day >= 1 && day <= 5)
+    return vacation ? "10:00" : "7:00";
+  else
+    return vacation ? "off" : "10:00";
+}
+
+
+/*
+The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6.
+Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a
+number.
+*/
+
+public boolean love6(int a, int b) {
+  if (a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6)
+    return true;
+  else
+    return false;
+}
+
+
+/*
+Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is true, in
+which case return true if the number is less or equal to 1, or greater or equal to 10.
+*/
+
+public boolean in1To10(int n, boolean outsideMode) {
+  if (!outsideMode)
+    return (n >= 1 && n <= 10) ? true : false;
+  else
+    return (n <= 1 || n >= 10) ? true : false;
+}
+
+
+/*
+We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+Return true if the given non-negative number is special.
+*/
+
+public boolean specialEleven(int n) {
+  int result = n % 11;
+  return result == 0 || result == 1 ? true : false;
+}
+
+
+/*
+Return true if the given non-negative number is 1 or 2 more than a multiple of 20.
+*/
+
+public boolean more20(int n) {
+  int result = n % 20;
+  return result == 1 || result == 2 ? true : false;
+}
+
+
+/*
+Return true if the given non-negative number is a multiple of 3 or 5, but not both.
+*/
+
+public boolean old35(int n) {
+  int result3 = n % 3;
+  int result5 = n % 5;
+
+  return (result3 == 0 && result5 != 0)
+    ? true
+    : (result5 == 0 && result3 != 0
+      ? true
+      : false);
+}
+
+
+/*
+Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38
+and 39 return true, but 40 returns false.
+*/
+
+public boolean less20(int n) {
+  int result = n % 20;
+  int diff = Math.abs(result - 20);
+
+  return diff == 1 || diff == 2 ? true : false;
+}
+
