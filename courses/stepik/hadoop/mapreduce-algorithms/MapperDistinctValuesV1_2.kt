31d70fab072999c3139a7b2dc9f567e2de5ef2bf
Roman ⛧ <rtereshkov@gmail.com>
2022-04-18 21:12:32 +0700

    Create MapperDistinctValuesV1_2.kt

---
+++ courses/stepik/hadoop/mapreduce-algorithms/MapperDistinctValuesV1_2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // put your code here
+    generateSequence(::readLine)
+        .joinToString("\n")
+        .split("\n")
+        .map {
+            it.trim().split(",").last()
+        }
+        .forEach(::printResult)
+}
+
+fun printResult(element: String) {
+    println("$element\t1")
+}
