03eb59d6770ccda5d5f2f17fcf72714bfe8e190e
Roman ⛧ <rtereshkov@gmail.com>
2022-04-18 21:09:20 +0700

    Create ReducerDistinctValuesV1.kt

---
+++ courses/stepik/hadoop/mapreduce-algorithms/ReducerDistinctValuesV1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // put your code here
+    generateSequence(::readLine)
+        .joinToString("\n")
+        .split("\n")
+        .map {
+            it.trim().split("\t").first()
+        }
+        .toSet()
+        .forEach(::println)
+}
